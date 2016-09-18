7a4a665666fb27f17c678c9ae7967331144cc755
SparkyCola <e.mail.mattis@googlemail.com>
2016-09-18 17:31:52 +0200

    finished transition to new display type

--- timeLapseOLED.py
+++ timeLapseOLED.py
@@ -28,7 +28,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 buttonEdge = GPIO.FALLING #FALLING: relase trigger, RISING: press trigger
 
-debug = True #show pressed buttons
+debug = False #show pressed buttons
 
 #------------------------------OLED-----------------------------
 
@@ -55,19 +55,19 @@ font = ImageFont.truetype(fontPath, 15)
 
 #image = Image.open('/home/pi/Adafruit_Python_SSD1306/examples/happycat_oled_64.ppm').convert('1')
 
-def drawBuffer(channel = False):
+def drawBuffer():
     # Draw the image buffer.
     disp.image(image)
     disp.display()
 
-def clearBuffer(channel = False):
+def clearBuffer():
     # Clear image buffer by drawing a black filled box.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
     nextX = [0,0]
 
 def writeStringToBuffer(line, text):
     textWidth, textHeight = draw.textsize(text, font=font)
-    draw.text((0, line * (textHeight + 3)), text, font=font, fill=255)
+    draw.text((0, line * (textHeight + 2)), text, font=font, fill=255)
 
 def clearDisplay():
     clearBuffer()
@@ -92,6 +92,7 @@ rightButtonPressed = False
 
 #-----------------------MENU-----------------------------------
 def startTimeLapse(duration, interval, raw):
+    global font, draw
     amountOfPictures = int(duration * 60 * 60 / interval)
     if(raw):
         estSize = 10.0
@@ -112,16 +113,36 @@ def startTimeLapse(duration, interval, raw):
         drawBuffer()
         raise FileNotFoundError('No possible prefix found', 'in: ' + pictureDir)
     fileName = prefix + '{0:0' + str(digits(amountOfPictures)) + 'd}.jpg'
+    #just for testing REḾOVE IT
+    #----------------
     sleep(5)
+
+    textWidth, textHeight = draw.textsize('0123456789', font=font)
+    thirdLineY = 2 * (textHeight + 2)
     for pic in range(0,amountOfPictures):
-        #lcd.clear()
-        #lcd.write_string(str(pic + 1) + '/' + str(amountOfPictures) + ' ' + str(round(pic + 1 / amountOfPictures * 100,1)) + '%')
-        #lcd.write_string('\r\n' + str(int((amountOfPictures - pic + 1) * interval / 60))  + 'min ')
+        clearBuffer()
+        writeStringToBuffer(0, str(pic + 1) + '/' + str(amountOfPictures) + ' ' + str(round(pic + 1 / amountOfPictures * 100,1)) + '%')
+        writeStringToBuffer(1, str(int((amountOfPictures - pic + 1) * interval / 60))  + 'min left')
+        drawBuffer()
         camera.capture(pictureDir + fileName.format(pic), format=pictureFormat, bayer=raw)
-        sleep(interval)
-    #lcd.clear()
-    #lcd.write_string('done.')
-    exit()
+        for i in range(0,interval):
+            sleep(1)
+            draw.rectangle((0, thirdLineY, textWidth, thirdLineY+textHeight), fill=0)
+            writeStringToBuffer(2, str(interval-i))
+            drawBuffer()
+
+    clearBuffer()
+    font = ImageFont.truetype(fontPath, 50)
+    writeStringToBuffer(0,'DONE.')
+    font = ImageFont.truetype(fontPath, 15)
+    drawBuffer()
+
+    #should shut down when quit is helt
+    while True:
+        if GPIO.input(27):
+            writeStringToBuffer(2.5,'shutting down.')
+            drawBuffer()
+            shutdown()
 
 menuItems = ['Duration', 'Interval', 'save raw data', 'start lapse']
 # unit, [min, step, max, default/current] (for float/int values)
@@ -177,6 +198,13 @@ def backButton(channel = False):
 def enterButton(channel = False):
     if debug: print('enter button pressed')
     menu.goDown()
+
+def shutdown():
+    command = "/usr/bin/sudo /sbin/shutdown -h now"
+    import subprocess
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    print(output)
 
 #Quit, Left, Right, Back, Enter Buttons
 GPIO.setup([23,18,17,22,27], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
