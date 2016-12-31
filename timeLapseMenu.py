ced9761ead6a633824d15a82cf6c15eb4aadee2b
SparkyCola <e.mail.mattis@googlemail.com>
2016-12-31 21:57:53 +0100

    did quite some refractoring

--- timeLapseMenu.py
+++ timeLapseMenu.py
@@ -8,6 +8,7 @@ class timeLapseMenu:
     width = 128
     height = 64
     classWideCapturing = False
+    items = []
 
     def drawBuffer(self):
         # Draw the image buffer.
@@ -50,14 +51,14 @@ class timeLapseMenu:
         self.drawBuffer()
 
     def __init__(self, theItems, theChoices, theDisplay, theDrawObject, theImage, theFont, theCamera):
-        global width, height
-        self.items =theItems
+        global width, height, items
+        items = theItems
         self.choices = theChoices
         self.font = theFont
         self.image = theImage
         self.camera = theCamera
         self.debug = True
-        if len(self.choices) != len(self.items):
+        if len(self.choices) != len(items):
             print('choices list does not fit the items list')
         self.disp = theDisplay
         self.draw = theDrawObject
@@ -72,127 +73,141 @@ class timeLapseMenu:
     def home(self):
         global currentItem, mode
         #self.lcd.clear()
-        self.printItem(0)
         currentItem = 0
-        mode = 0
+        self.setMode(0)
 
     def leftPressed(self):
         global currentItem
         if not self.classWideCapturing:
             if mode == 0:
-                if currentItem == 0:
-                    next_Item = len(self.items) - 1
-                else:
-                    next_Item = currentItem - 1
-                currentItem = next_Item
-                self.printItem(next_Item)
+                self.goToLastItem(printUpdate=True)
             #Value Mode
             else:
                 if self.debug: print('setting value -')
                 self.setItemValue(currentItem, -1)
                 #update the display
-                self.printItem(currentItem, False)
+                self.updateItem()
 
     def rightPressed(self):
-        global currentItem
+        global currentItem, mode
         if not self.classWideCapturing:
             if mode == 0:
-                if currentItem == len(self.items) - 1:
-                    next_Item = 0
-                else:
-                    next_Item = currentItem + 1
-                currentItem = next_Item
-                self.printItem(next_Item)
+                self.goToNextItem(printUpdate=True)
             #Value Mode
             else:
                 if self.debug: print('setting value +')
                 self.setItemValue(currentItem, 1)
                 #update the display
-                self.printItem(currentItem, False)
+                self.updateItem()
+
+    def goToNextItem(self, printUpdate=False):
+        global currentItem, items
+        if currentItem == len(items) - 1:
+            next_Item = 0
+        else:
+            next_Item = currentItem + 1
+        currentItem = next_Item
+        if printUpdate: self.updateItem()
+
+    def goToLastItem(self, printUpdate=False):
+        global currentItem, items
+        if currentItem == 0:
+            next_Item = len(items) - 1
+        else:
+            next_Item = currentItem - 1
+        currentItem = next_Item
+        if printUpdate: self.updateItem()
 
     def enterPressed(self):
         global mode
         if not self.classWideCapturing:
             if mode is 1:
-                if self.debug: print('in set mode, going to the next value (right)')
-                mode = 0
+                if self.debug: print('in set mode, going to the next value AFTER IMPLEMENTED (right)')
+                #self.goToNextItem(printUpdate=True)
             else:
                 if self.debug: print('going down into set mode')
                 #"delete" arrows
-                self.printItem(currentItem, False)
-                mode = 1
+                self.updateItem()
+                self.setMode(1)
 
     def goBack(self):
         global mode
         if not self.classWideCapturing:
             if mode is 1:
-                mode = 0
+                self.setMode(0)
+                updateItem()
+
 
     def backPressed(self):
         global mode, currentItem, debug
         if not self.classWideCapturing:
             if mode is 1:
                 if self.debug: print('going into menu mode')
-                self.printItem(currentItem)
-                mode = 0
+                self.setMode(0)
             elif mode is 0:
                 if self.debug: print('already in menu mode')
 
-    def printItem(self, itemID, showArrows = True):
+    def updateItem(self):
+        global items, currentItem, mode
         self.clearBuffer()
-        if itemID != 0 and showArrows:
+        if mode is 0:
+            showArrows = True
+        else:
+            showArrows = False
+
+        if currentItem != 0 and showArrows:
             showLeftArrow = True
         else:
             showLeftArrow = False
 
-        if itemID == len(self.items) - 1 or showArrows == False:
+        if currentItem == len(items) - 1 or showArrows == False:
             showRightArrow = False
         else:
             showRightArrow = True
 
-        self.writeStringToBuffer(0, self.items[itemID], aligment = 'centered', arrows = (showLeftArrow, showRightArrow), instantDisplay=False)
+        self.writeStringToBuffer(0, items[currentItem], aligment = 'centered', arrows = (showLeftArrow, showRightArrow), instantDisplay=False)
         #self.lcd.cursor_pos = (1,0)
-        if type(self.choices[itemID][1][0]) is str:
-            if type(self.choices[itemID][1][1]) is str:
-                currentElementID = len(self.choices[itemID][1]) - 1
-                self.writeStringToBuffer(1,self.choices[itemID][1][self.choices[itemID][1][currentElementID]], aligment = 'left')
-            if callable(self.choices[itemID][1][1]):
+        if type(self.choices[currentItem][1][0]) is str:
+            if type(self.choices[currentItem][1][1]) is str:
+                currentElementID = len(self.choices[currentItem][1]) - 1
+                self.writeStringToBuffer(1,self.choices[currentItem][1][self.choices[currentItem][1][currentElementID]], aligment = 'left')
+            if callable(self.choices[currentItem][1][1]):
                 useless = True
-                self.writeStringToBuffer(1,self.choices[itemID][1][0], aligment = 'left')
+                self.writeStringToBuffer(1,self.choices[currentItem][1][0], aligment = 'left')
         else:
-            self.writeStringToBuffer(1,str(round(self.choices[itemID][1][3],1)), aligment = 'left')
-        self.writeStringToBuffer(1,self.choices[itemID][0], aligment = 'right', instantDisplay = True)
+            self.writeStringToBuffer(1,str(round(self.choices[currentItem][1][3],1)), aligment = 'left')
+        self.writeStringToBuffer(1,self.choices[currentItem][0], aligment = 'right', instantDisplay = True)
 
-    def setItemValue(self, itemID, factor):
-        if type(self.choices[itemID][1][1]) is str:
-            lastStringElement = len(self.choices[itemID][1]) - 2
-            currentElement = self.choices[itemID][1][lastStringElement + 1]
+    def setItemValue(self, currentItem, factor):
+        if type(self.choices[currentItem][1][1]) is str:
+            lastStringElement = len(self.choices[currentItem][1]) - 2
+            currentElement = self.choices[currentItem][1][lastStringElement + 1]
             if currentElement + factor <= lastStringElement   and currentElement + factor >= 0:
-                self.choices[itemID][1][lastStringElement + 1] = currentElement + factor
+                self.choices[currentItem][1][lastStringElement + 1] = currentElement + factor
             else:
                 if currentElement + factor < 0:
-                    self.choices[itemID][1][lastStringElement + 1] = lastStringElement
+                    self.choices[currentItem][1][lastStringElement + 1] = lastStringElement
                 if currentElement + factor > lastStringElement:
-                    self.choices[itemID][1][lastStringElement + 1] = 0
-            if itemID == 2:
-                currentStringValue = self.choices[itemID][1][ self.choices[itemID][1][lastStringElement + 1] ]
+                    self.choices[currentItem][1][lastStringElement + 1] = 0
+            if currentItem == 2:
+                currentStringValue = self.choices[currentItem][1][ self.choices[currentItem][1][lastStringElement + 1] ]
                 self.camera.shutter_speed = int(float(sum(Fraction(s) for s in currentStringValue.split())) * 10**6)
 
-        if type(self.choices[itemID][1][1]) is int or type(self.choices[itemID][1][1]) is float:
-                minValue = self.choices[itemID][1][0]
+        if type(self.choices[currentItem][1][1]) is int or type(self.choices[currentItem][1][1]) is float:
+                minValue = self.choices[currentItem][1][0]
                 #make the step positive or negative depending on factor
-                valueStep = self.choices[itemID][1][1] * factor
-                maxValue = self.choices[itemID][1][2]
-                currentValue = self.choices[itemID][1][3]
+                valueStep = self.choices[currentItem][1][1] * factor
+                maxValue = self.choices[currentItem][1][2]
+                currentValue = self.choices[currentItem][1][3]
                 if currentValue + valueStep <= maxValue and currentValue + valueStep >= minValue:
-                    self.choices[itemID][1][3] += valueStep
+                    self.choices[currentItem][1][3] += valueStep
                 else:
                     if abs(currentValue - maxValue) <= 0.01:
-                        self.choices[itemID][1][3] = minValue
+                        self.choices[currentItem][1][3] = minValue
                     if abs(currentValue - minValue) <= 0.01:
-                        self.choices[itemID][1][3] = maxValue
+                        self.choices[currentItem][1][3] = maxValue
 
-        if callable(self.choices[itemID][1][1]):
+        if callable(self.choices[currentItem][1][1]):
             duration = self.choices[0][1][3]
             interval = self.choices[1][1][3]
             if self.choices[3][1][2] == 0:
@@ -200,10 +215,15 @@ class timeLapseMenu:
             else:
                 raw = False
             classWideCapturing = True
-            self.choices[itemID][1][1](duration, interval, raw)
+            self.choices[currentItem][1][1](duration, interval, raw)
     def capturing(self):
         return self.classWideCapturing
 
     def getMode(self):
         global mode
         return mode
+
+    def setMode(self, newMode, updateItem = True):
+        global mode
+        mode = newMode
+        if updateItem: self.updateItem()
